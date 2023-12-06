4207e1ac17de9b668e5761f7887fecfe4c7fab10
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-12-07 00:52:12 +0100

    [FEAT] Basic form and styles

---
+++ app/form.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export const Form = () => {
+	return (
+		<form
+			className="flex flex-col gap-4"
+			onSubmit={(evt) => {
+				evt.preventDefault()
+
+				const form = evt.currentTarget as HTMLFormElement
+				const url = form.elements.namedItem('url') as HTMLInputElement
+
+				console.log(url.value)
+			}}
+		>
+			<Label htmlFor="url">Image URL</Label>
+			<Input name="url" id="url" type="url" placeholder="https:://your-screenshoot/image.png"></Input>
+			<Button>Generate image code</Button>
+		</form>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Form } from './form'
+
 export default function Home() {
-	return <main className="">Hola mundo</main>
+	return (
+		<div className="grid grid-cols-[400px_1fr]">
+			<aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-900">
+				<header className="text-center">
+					<h1 className="text-3xl font-semibold">Image2Code</h1>
+					<h2 className="text-sm opacity-75">From an Image to Code using OpenAI GPT last tech</h2>
+				</header>
+				<section> {/*Filters placeholder*/}</section>
+			</aside>
+			<main className="bg-gray-900">
+				<section className="max-w-2xl mx-auto p-10">
+					<Form />
+				</section>
+			</main>
+			<footer>Created by Adrian Ferrandis</footer>
+		</div>
+	)
 }
